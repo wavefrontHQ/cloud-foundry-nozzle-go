c095bcc6e7437c7fb941c37d83b52b3b823e5999
German Laullon <laullon@gmail.com>
2019-04-09 20:02:26 +0200

    Filters with spaces on around the comma separator (#2)

--- nozzle/filter_test.go
+++ nozzle/filter_test.go
@@ -1,51 +1,53 @@
-package nozzle
+package nozzle_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/wavefronthq/cloud-foundry-nozzle-go/nozzle"
 )
 
 func TestNoFilters(t *testing.T) {
-	filters := &Filters{}
-	glob := NewGlobFilter(filters)
+	filters := &nozzle.Filters{}
+	glob := nozzle.NewGlobFilter(filters)
 
 	assert.True(t, glob.Match("ok.metric.1", nil))
 	assert.True(t, glob.Match("ko.metric.1", nil))
 }
 
 func TestEmptyFilterString(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.MetricsWhiteList = []string{""}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	assert.True(t, glob.Match("ok.metric.1", nil))
 	assert.True(t, glob.Match("ko.metric.1", nil))
 }
 
 func TestWhiteList(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.MetricsWhiteList = []string{"pcf.bosh-*-forwarder*"}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	assert.True(t, glob.Match("pcf.bosh-hm-forwarder.system.cpu.user.Load", nil))
 	assert.False(t, glob.Match("pcf.container.rep.memory_bytes_quota", nil))
 }
 
 func TestBlackList(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.MetricsBlackList = []string{"ko.*"}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	assert.True(t, glob.Match("ok.metric.1", nil))
 	assert.False(t, glob.Match("ko.metric.1", nil))
 }
 
 func TestWhiteAndBlackList(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.MetricsWhiteList = []string{"ok.*"}
 	filters.MetricsBlackList = []string{"*.ko"}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	assert.True(t, glob.Match("ok.metric.1", nil))
 	assert.False(t, glob.Match("ok.metric.1.ko", nil))
@@ -53,10 +55,10 @@ func TestWhiteAndBlackList(t *testing.T) {
 }
 
 func TestTagsWhiteAndBlackList(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.MetricsTagWhiteList = map[string][]string{"tag1": {"ok.*"}, "tag2": {"ok.*"}}
 	filters.MetricsTagBlackList = map[string][]string{"tag1": {"*.ko"}}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	assert.False(t, glob.Match("", map[string]string{"tag1": "tururu"}))
 	assert.True(t, glob.Match("", map[string]string{"tag1": "tururu", "tag2": "ok.tururu"}))
@@ -66,9 +68,9 @@ func TestTagsWhiteAndBlackList(t *testing.T) {
 }
 
 func TestTagInclude(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.TagInclude = []string{"tag[0-9]"}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	tags := map[string]string{"tag1": "tururu", "tagA": "tururu", "tag": "tururu"}
 	glob.Match("", tags)
@@ -76,11 +78,28 @@ func TestTagInclude(t *testing.T) {
 }
 
 func TestTagExclude(t *testing.T) {
-	filters := &Filters{}
+	filters := &nozzle.Filters{}
 	filters.TagExclude = []string{"tag[0-9]"}
-	glob := NewGlobFilter(filters)
+	glob := nozzle.NewGlobFilter(filters)
 
 	tags := map[string]string{"tag1": "tururu", "tagA": "tururu", "tag": "tururu"}
 	glob.Match("", tags)
 	assert.Equal(t, 2, len(tags))
+}
+
+func TestFiltersWithSpaces(t *testing.T) {
+	os.Clearenv()
+	setUpFooEnv()
+
+	os.Setenv("FILTER_METRICS_WHITE_LIST", "foo1 , foo2,foo3 ")
+
+	config, err := nozzle.ParseConfig()
+	if err != nil {
+		assert.FailNow(t, "[ERROR] Unable to build config from environment: ", err)
+	}
+
+	filter := nozzle.NewGlobFilter(config.Wavefront.Filters)
+	assert.True(t, filter.Match("foo1", nil))
+	assert.True(t, filter.Match("foo2", nil))
+	assert.True(t, filter.Match("foo3", nil))
 }
