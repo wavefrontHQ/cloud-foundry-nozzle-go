4d19794ae2b08b7ae96bad5c853d9c285941819a
prydin <prydin@vmware.com>
2019-04-11 06:57:14 -0400

    Added comments

--- nozzle/cache.go
+++ nozzle/cache.go
@@ -5,6 +5,10 @@ import "time"
 // Cache is an abstract representation of a cache capable of holding e.g. AppInfo records. It must
 // support expiration of cache items.
 type Cache interface {
+	// Get returns the value for the key if it exists. If it doesn't exist, (nil, false) is returned.
 	Get(key string) (interface{}, bool)
+
+	// Set puts a key-value pair into the cache. Depending on the implementation, items may be evicted
+	// from the cache if it's already at capacity.
 	Set(key string, value interface{}, ttl time.Duration)
 }
