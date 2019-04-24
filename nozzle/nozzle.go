55bd5760a868131bd53e9dc5d43d8ee075367585
German Laullon <glaullon@vmware.com>
2019-04-24 11:18:17 +0200

    Forcing restart on `Invalid authorization`

--- nozzle/nozzle.go
+++ nozzle/nozzle.go
@@ -95,4 +95,5 @@ func (s *forwardingNozzle) handleEvent(envelope *events.Envelope) {
 
 func (s *forwardingNozzle) handleError(err error) {
 	logger.Printf("Error from firehose - %v", err)
+	logger.Fatal("Forcing restart")
 }
